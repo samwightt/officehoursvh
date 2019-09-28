54705ff6c5423481107361f96b68f6808a1226ac
samuelwight <samuelwight@gmail.com>
2019-09-28 14:36:46 -0400

    Add websockets setting stuff. great!

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,3 +1,32 @@
-export default {
+let queue: string[] = []
 
+let websocketSendFunction: ((message: string) => any) | null = null
+
+let sendFunction = (message: string) => {
+  if (websocketSendFunction) websocketSendFunction(message)
+  else queue.push(message)
+}
+
+export default {
+  establishConnection({ commit, state }) {
+    let socket = new WebSocket('ws://localhost:8090')
+
+    socket.onmessage = (m) => {
+      commit('setTree', JSON.parse(m.data))
+    }
+
+    socket.onopen = () => {
+      if (queue.length > 0) {
+        queue.forEach(item => socket.send(item))
+        queue = []
+      }
+      websocketSendFunction = (message: string) => {
+        socket.send(message)
+      }
+      socket.send('__load__')
+    }
+  },
+  sendMessage({ commit }, message: { [key: string]: any }) {
+    sendFunction(JSON.stringify(message))
+  }
 }
--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,3 +1,7 @@
 export default {
-
+  setTree(state, treeToSet) {
+    state.users = treeToSet.users
+    state.conversations = treeToSet.conversations
+    state.tags = treeToSet.tags
+  },
 }
